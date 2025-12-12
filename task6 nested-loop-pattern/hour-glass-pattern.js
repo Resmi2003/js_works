8c9621fd2f2897cc0228c748de83fb47341a68d0
resmi <resmirmenonrrm@gmail.com>
2025-12-12 19:48:33 +0530

    m

--- task6 nested-loop-pattern/hour-glass-pattern.js
+++ task6 nested-loop-pattern/hour-glass-pattern.js
@@ -15,7 +15,7 @@ for(let row=1;row<=7;row++){
 
     for(let column=1;column<=9;column++){
 
-        if(row==1 && column==1 || row==1 && column==3 || row==1 && column==5 || row==1 && column==7 || row==1 && column==9 || row==2 && column==2 || row==2 && column==4 || row==2 && column==6 || row==2 && column==8 || row==3 && column==3 || row==3 && column==5 || row==4 && column==4 || row==5 && column==3 || row==5 && column==5 || row==6 && column==2 || row==6 && column==4 || row==6 && column==6 || row==6 && column==8 || row==7 && column==1 || row==7 && column==3 || row==7 && column==5 || row==7 && column==7 || row==7 && column==9){
+        if(row==1 && column==1 || row==1 && column==3 || row==1 && column==5 || row==1 && column==7 || row==1 && column==9 || row==2 && column==2 || row==2 && column==4 || row==2 && column==6 || row==2 && column==8 || row==3 && column==3 || row==3 && column==7 || row==4 && column==5 || row==5 && column==3 || row==5 && column==7 || row==6 && column==2 || row==6 && column==4 || row==6 && column==6 || row==6 && column==8 || row==7 && column==1 || row==7 && column==3 || row==7 && column==5 || row==7 && column==7 || row==7 && column==9){
             Pattern += "*";
         }
 
