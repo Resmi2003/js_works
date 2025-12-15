6ddc44ea6ba7003192a5c423319f59751f028ca0
resmi <resmirmenonrrm@gmail.com>
2025-12-15 10:30:49 +0530

    modified

---
+++ my-works/string-works/intro.js
@@ -0,0 +1,37 @@
+var companyName = " Luminar Technolab ";
+// index-          0123456789012345678
+
+console.log(companyName.length);
+
+console.log(companyName.toUpperCase());  
+console.log(companyName.toLowerCase());
+
+console.log(companyName.charAt(2));
+
+console.log(companyName.at(-2));
+
+console.log(companyName.concat(".com",".edu"));   
+
+console.log(companyName.slice(0,7));  
+console.log(companyName.slice(8,17));   
+console.log(companyName.slice(-3));  
+
+console.log(companyName.substring(0,8));  
+
+console.log(`new string:${companyName.trim()}....`);  
+
+console.log(`new string:${companyName.trimStart()}....`);   
+console.log(`new string:${companyName.trimEnd()}....`);   
+
+console.log(companyName.padEnd(25,"$"));    
+
+
+
+
+
+
+
+
+
+
+
