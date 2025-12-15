635c2987e6acf2251e299d6d6c484d55676b92bf
resmi <resmirmenonrrm@gmail.com>
2025-12-15 21:42:49 +0530

    m

--- task8 loop and string/javascript-loop-questions/nested-loop-pattern.js
+++ task8 loop and string/javascript-loop-questions/nested-loop-pattern.js
@@ -5,35 +5,32 @@
 // 123
 // 1234
 
- for(let row=1;row<=4;row++){
-    
+for (let row = 1; row <= 4; row++) {
+
     let pattern = "";
 
+    for (let column = 1; column <= 4; column++) {
 
-    for(let column=1;column<=4;column++){
-
-        if(column==1){
+        if (column == 1) {
             pattern += "1";
         }
 
-        else if(column==2 && row!=1){
+        else if (column == 2 && row != 1) {
             pattern += "2";
         }
 
-        else if(column==3 && row!=1 && row!=2){
+        else if (column == 3 && row != 1 && row != 2) {
             pattern += "3";
         }
 
-        else if(row==4 && column==4){
+        else if (row == 4 && column == 4) {
             pattern += "4";
         }
 
-        else{
+        else {
             pattern += " ";
         }
     }
 
-    
     console.log(pattern);
-    
- }
+}
